f658b70f02cea28f49e6bbc7d939d1b5bd8f54e8
marcbarretoe <31424201+marcbarretoe@users.noreply.github.com>
2020-02-06 20:03:55 -0300

    V1

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+/*import logo from './logo.svg';*/
+import './App.css';
+import About from './paginas/About';
+import Drawer from './paginas/Drawer';
+import Formulario from './paginas/Formulario';
+import ProfileForm from './paginas2/ProfileForm';
+/*import NestedList from './paginas/NestedList';*/
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+  
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+//import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <Drawer />
+    </div>
+  );
+}
+
+
+//export default App;
---
+++ src/paginas2/DefinicionParametros.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Formik } from 'formik';
+import ProfileSchema from './ProfileSchema';
+
+function DefinicionParametros() {
+  return (
+    <div>
+      <h2> hola</h2>
+    </div>
+
+  );
+}
+export default DefinicionParametros;
